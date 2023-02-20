7143c3b1c26642afeec734ed3591e0e4aac0c329
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-02-20 20:34:19 +0100

    comments

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,14 @@ import newsRouter from './news/news.router';
 const app = express();
 
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/news", newsRouter)
-// app.use("/api/news", dynamicNewsRouter(startDate, endDate))
 
-//Placeholder dates
-// const startDate = new Date('February 18, 2022 03:24:00')
-// const endDate = new Date('February 20, 2024 03:24:00')
+
+// Use router in routers
+app.use("/api/news", newsRouter)
+
 
 app.listen(Number(3000), () => {
     console.log("Listening on port 3000")
--- src/news/news.router.ts
+++ src/news/news.router.ts
@@ -5,37 +5,25 @@ import {body, validationResult} from "express-validator";
 import * as newsService from "./news.service";
 
 
-//GET: Label and region for selected dates. 
+
 const newsRouter = express.Router();
 
 
-
+//GET: Label and region for selected dates, specified via query string. 
 export default newsRouter.get("/", async (request: Request, response: Response) => {
     try {
         // Wtf is as {startDate: string}. I don't get this line, got help from good friend chatGPT
+        // Take the query params anduse in prisma query.
         const startDate = new Date((request.query as { startDate: string }).startDate);
         const endDate = new Date((request.query as { endDate: string }).endDate);
 
+
+        // Call prisma query
         const news = await newsService.getSentiments(startDate, endDate)
+
+
         return response.status(200).json(news)
     } catch (error: any) {
         return response.status(500).json(error.message)
     }
 })
-
-
-
-// export const dynamicNewsRouter = (startDate: Date, endDate: Date) => {
-//     const newsRouter = express.Router();
-
-//     newsRouter.get("/", async (request: Request, response: Response) => {
-//         try {
-//             const news = await newsService.getSentiments(startDate, endDate)
-//             return response.status(200).json(news)
-//         } catch (error: any) {
-//             return response.status(500).json(error.message)
-//         }
-//     })
-
-//     return newsRouter
-// }
